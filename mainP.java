e2a68700404e9961ae74662c0e641b48ad9a5e28
Himantha Hirushan <himanthad4@gmail.com>
2022-12-08 04:00:19 +0530

    start the project

---
+++ mainP.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+class mainP{
+
+    
+public static void main (String args[]){
+
+
+Scanner sc = new Scanner(System.in);
+
+
+System.out.println("enter number 01 :");
+int n1 =  sc.nextInt();
+
+System.out.println("enter number 02 :");
+int n2 =  sc.nextInt();
+
+
+
+}
+}
