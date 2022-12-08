cb61dc6e96cf29f62f0d45723906cb28b0bb7800
Himantha Hirushan <himanthad4@gmail.com>
2022-12-08 20:31:27 +0530

    add calculation class and implement some methods

--- mainP.java
+++ mainP.java
@@ -9,15 +9,32 @@ public static void main (String args[]){
 Scanner sc = new Scanner(System.in);
 
 
-System.out.println("enter number 01 :");
+
+System.out.print("enter number 01 :");
 int n1 =  sc.nextInt();
 
-System.out.println("enter number 02 :");
+System.out.print("enter number 02 :");
 int n2 =  sc.nextInt();
 
+calculation c1 = new calculation();
 
 
+System.out.println("what do you want (add-press 1),(multiply-press 3)");
+int calMethod = sc.nextInt();
 
-
+if(calMethod == 1){
+           c1.add(n1,n2); 
 }
+
+
+else if(calMethod == 3){
+          c1.multiply(n1,n2); 
 }
+
+
+
+}}
+
+
+
+
